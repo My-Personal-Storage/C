72f76964cbe2f308da5dbf3ad1c12aa09f650556
Farhan <118242884+FarhanGinting@users.noreply.github.com>
2025-06-27 10:20:33 +0700

    feat (finish) : Menambahkan file constants.cpp dan constants.exe yang menunjukkan cara mendeklarasikan dan menggunakan konstanta dalam C++.

---
+++ 5.Constants/constants.cpp
@@ -0,0 +1,12 @@
+# include <stdio.h>
+
+int main() {
+    //  When you declare a constant, you must be assign a value to it.
+    const int minutesPerHour = 60;
+    
+    // Good Practice
+    // constant variable good practice to declare them with uppercase letters.
+    const int BIRTHYEAR = 1990;
+
+    return 0;
+}
